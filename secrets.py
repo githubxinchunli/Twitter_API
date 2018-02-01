477c7253835bfe7ca58ba64962d6c64b3810d73c
Xinchun Li <xincli@umich.edu>
2018-02-01 15:35:14 -0500

    Add files via upload

---
+++ secrets.py
@@ -0,0 +1,8 @@
+
+
+api_key = "uNbBQ5PrLRPXZNqAOlNN3arL0"
+api_secret = "Zivx9xctWECoPMJsp3DekA43LJkWQL7uBCRo3ApncftOMszFlx"
+
+access_token = "758274513993867265-pW6njrWW1zkJ9WTdqashBJeASsJYdjD"
+access_token_secret = "cOVDab2XD9JLfPg37r6Ui9XX6qMhDAxzrjS4mgxJQ9G73"
+
