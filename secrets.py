56c2d32a4c9a909cdac16b03433fbbc4efd09005
Xinchun Li <xincli@umich.edu>
2018-02-06 18:46:44 -0500

    Update secrets.py

--- secrets.py
+++ secrets.py
@@ -1,7 +1,7 @@
 
 
-api_key = "uNbBQ5PrLRPXZNqAOlNN3arL0"
-api_secret = "Zivx9xctWECoPMJsp3DekA43LJkWQL7uBCRo3ApncftOMszFlx"
+client_key = "uNbBQ5PrLRPXZNqAOlNN3arL0"
+client_secret = "Zivx9xctWECoPMJsp3DekA43LJkWQL7uBCRo3ApncftOMszFlx"
 
 access_token = "758274513993867265-pW6njrWW1zkJ9WTdqashBJeASsJYdjD"
 access_token_secret = "cOVDab2XD9JLfPg37r6Ui9XX6qMhDAxzrjS4mgxJQ9G73"
