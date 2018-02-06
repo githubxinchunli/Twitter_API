c1bc1084cca6b676fa118f2980c83fac1a05c5fc
Xinchun Li <xincli@umich.edu>
2018-02-06 18:46:14 -0500

    Add files via upload

---
+++ oauth-twitter-withkeys_modified.py
@@ -0,0 +1,30 @@
+
+from requests_oauthlib import OAuth1Session
+import secrets
+import json
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.buffer,encoding='gb18030') # prevent encoding error
+
+client_key = secrets.client_key
+client_secret = secrets.client_secret
+
+resource_owner_key = secrets.access_token
+resource_owner_secret = secrets.access_token_secret
+
+protected_url = 'https://api.twitter.com/1.1/account/settings.json'
+
+oauth = OAuth1Session(client_key,
+                          client_secret=client_secret,
+                          resource_owner_key=resource_owner_key,
+                          resource_owner_secret=resource_owner_secret)
+
+protected_url = 'https://api.twitter.com/1.1/search/tweets.json'
+params = {'q':'food'}
+r = oauth.get(protected_url, params=params)
+
+results_list = json.loads(r.text)["statuses"]
+for result in results_list:
+                          print(result['user']['name'])
+                          print(result['text'])
